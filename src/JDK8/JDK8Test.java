a5ac3dffe18a62b9d2216a1cb64d6ebee7880c2e
Salexal <923855206@qq.com>
2019-04-23 09:28:13 +0800

    md 更新了内容  新增 接口的默认方法和静态方法  （接下来整理lambda）

---
+++ src/JDK8/JDK8Test.java
@@ -0,0 +1,15 @@
+package JDK8;
+
+/**
+ * @Author: Salexal.fww
+ * @Date: 2019/4/22 21:19
+ * @Version 1.0
+ * @Type
+ */
+public class JDK8Test {
+
+
+    public static void main(String[] args) {
+
+    }
+}
